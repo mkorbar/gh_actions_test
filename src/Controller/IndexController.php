22f3c6480580470988adc425f7f7f756d7b502ea
Matej Korbar <matej.korbar@dlabs.si>
2021-11-03 14:39:32 +0100

    Create an index page, using twig template and annotations, both installed with composer

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/IndexController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class IndexController extends AbstractController
+{
+    /**
+     * @return Response
+     * @throws \Exception
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
